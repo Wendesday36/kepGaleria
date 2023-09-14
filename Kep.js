f94378851871ebf1d5d3e03daef7a7ba8e86bf56
Wendesday36 <bertazoeanna@gmail.com>
2023-09-14 13:22:55 +0200

    kepek1/3

---
+++ Kep.js
@@ -0,0 +1,17 @@
+export const KepekLista = [
+    {
+        cim: "halloween",
+        kep: "kepek/halloween.jpg",
+        leiras: "old school",
+    },
+    {
+        cim: "Hello Kitty",
+        kep: "kepek/hello.jpg",
+        leiras: "hello kitty",
+    },
+    {
+        cim: "napraforgo",
+        kep: "kepek/napraforgo.jpg",
+        leiras: "napraforgo",
+    },
+];
