502e7ccfe4433f8acad69635244f9afed68076ad
PappNiki20 <papp.nikoletta001@gmail.com>
2023-09-14 13:28:26 +0200

    niki kepek listaban

--- Kep.js
+++ Kep.js
@@ -14,4 +14,19 @@ export const KepekLista = [
         kep: "kepek/napraforgo.jpg",
         leiras: "napraforgo",
     },
+    {
+        cim: "Velaris",
+        kep: "kepek/letöltés.jpg",
+        leiras: "város",
+    },
+    {
+        cim: "Acotar",
+        kep: "kepek/images (1).jpg",
+        leiras: "szereplők",
+    },
+    {
+        cim: "főszereplők",
+        kep: "kepek/images.jpg",
+        leiras: "Rhys Feyre",
+    },
 ];
