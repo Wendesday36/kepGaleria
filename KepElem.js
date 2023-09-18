c6715cc98e83cbc14a32862de3173403e400f74d
PappNiki20 <papp.nikoletta001@gmail.com>
2023-09-18 20:27:07 +0200

    modal megjelnik, szepen mukodik

--- KepElem.js
+++ KepElem.js
@@ -6,7 +6,7 @@ class KepElem {
     this.#sor();
     this.sorElem = this.tablaElem.find("div:last-child");
     this.gombElem = this.tablaElem.find("button:last-child");
-    //const BTTN = $(`.kedvenc:last-child`);
+   
     this.sorElem.on("click", () => {
       this.esemenyTrigger();
       this.OpenBootstrapPopup();
@@ -20,37 +20,15 @@ class KepElem {
     for (const key in this.#kep) {
       const element = this.#kep[key];
       if (key === "kep") {
-        txt += `<div ><img src ="${element}" alt="">
-        <div class="modal" id="myModal">
-        <div class="modal-dialog">
-          <div class="modal-content">
-      
-            <!-- Modal Header -->
-            <div class="modal-header">
-              <h3 class="modal-title">${this.#kep.cim}</h3>
-              <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-            </div>
-      
-            <!-- Modal body -->
-            <div class="modal-body">
-            <img src ="${element}" alt="">
-            <br>
-           Leírás:   ${this.#kep.leiras}
-            </div>
-      
-            <!-- Modal footer -->
-            <div class="modal-footer">
-            </div>
-      
-          </div>
-        </div>
-      </div>
-        
-        </div>`;
+        txt += `
+          <div >
+            <img src="${element}" alt="">
+          </div>`;
       }
     }
 
     this.tablaElem.append(txt);
+    
   }
   esemenyTrigger() {
     console.log(this.#kep.cim);
@@ -61,13 +39,21 @@ class KepElem {
     // console.log("KepElem osztaly: esemenyTrigger() hivas");
   }
   OpenBootstrapPopup() {
+    // Frissítsd a modal címét és tartalmát
+    $("#myModalLabel").text(this.#kep.cim);
+    $("#myModal .modal-body").html(`
+      <img src="${this.#kep.kep}" alt="">
+      <br>
+      Leírás: ${this.#kep.leiras}
+    `);
+
+    // Nyisd meg a modal ablakot
     $("#myModal").modal("show");
   }
+
   CloseBootstrapPopup() {
-   // $("#myModal").modal("close");
-    //window.parent.$('#myModal').dialog('close');
-   // $(this).closest('#myModal').dialog('close'); 
-   $('#myModal').modal('toggle');
+    // Bezárás a Bootstrap modal szabályai szerint
+    $("#myModal").modal("hide");
   }
 }
 export default KepElem;
